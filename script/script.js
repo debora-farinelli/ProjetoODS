fa75eb981e64bc9d0c3a3e189d423243a5d68e32
Débora Farinelli <deborafarinelli2005@hotmail.com>
2024-10-28 15:34:33 -0300

    projectODS

---
+++ script/script.js
@@ -0,0 +1,85 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", function() {
+    let botao = document.querySelectorAll(".item")
+
+    botao.forEach(botao => {
+        botao.addEventListener("mouseover", function() {
+            botao.style.backgroundColor = "#FE5359"
+        })
+
+        botao.addEventListener("mouseout", function() {
+            botao.style.backgroundColor = "#f5a5a5"
+        })
+
+        botao.addEventListener("click", function() {
+            botao.style.backgroundColor = "blue"
+        })
+
+        botao.addEventListener("mousedown", function() {
+            botao.style.backgroundColor = "blue"
+        })
+
+     })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let submitDoacao = document.querySelector("#submitDoacao")
+
+    submitDoacao.addEventListener("mouseover", function() {
+        submitDoacao.style.backgroundColor = "blue"
+        submitDoacao.style.color = "white"
+
+        submitDoacao.addEventListener("mouseout", function() {
+            submitDoacao.style.backgroundColor = "white"
+            submitDoacao.style.color = "black"
+        })
+
+        submitDoacao.addEventListener("mousedown", function() {
+            submitDoacao.style.backgroundColor = "green"
+            submitDoacao.style.color = "white"
+        })
+
+        submitDoacao.form.addEventListener("submit", function(event) {
+            event.preventDefault()
+            redirecionar()
+        })
+        
+    })
+})
+
+function redirecionar() {
+    window.location.href='../Pags_Doacao/indexEnviar.html'
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    let itemHistorico = document.querySelector(".itemHistorico")
+
+    itemHistorico.addEventListener("mouseover", function() {
+        itemHistorico.style.backgroundColor = "green"
+    })
+
+    itemHistorico.addEventListener("mouseout", function() {
+        itemHistorico.style.backgroundColor = "#ffcc00"
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let animacao = document.querySelector(".right-content")
+
+    animacao.addEventListener("mouseout", function() {
+        animacao.style.backgroundColor = "#ffeef1"
+    })
+})
+
+document.addEventListener("DOMContentLoaded", function() {
+    let animacaoTabela = documento.querySelector(".tr")
+
+    animacaoTabela.addEventListener("mouseover", function() {
+        animacaoTabela.style.backgroundColor = "#f2f2f2;"
+    })
+
+    animacaoTabela.addEventListener("mouseout", function() {
+        animacaoTabela.style.backgroundColor = "white"
+    })
+})
